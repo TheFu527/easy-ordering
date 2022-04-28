ea50c7fe01451d573c8dbabb8327b6ffa62fdb39
dingjikerbo <dingjikerbo@gmail.com>
2022-04-28 10:51:56 -0700

    restful api

--- business/src/main/java/org/neu/cs6650/koi/business/controller/BusinessController.java
+++ business/src/main/java/org/neu/cs6650/koi/business/controller/BusinessController.java
@@ -1,5 +1,6 @@
 package org.neu.cs6650.koi.business.controller;
 
+import org.apache.tomcat.util.http.parser.MediaType;
 import org.neu.cs6650.koi.business.service.BusinessService;
 import org.neu.cs6650.koi.common.dto.AccountDTO;
 import org.neu.cs6650.koi.common.dto.BusinessDTO;
@@ -24,28 +25,63 @@ public class BusinessController {
         return businessService.handleBusiness(businessDTO);
     }
 
-    @GetMapping("/getStock")
-    ObjectResponse getStock(@RequestParam String commodityCode) {
-        CommodityDTO commodityDTO = new CommodityDTO();
-        commodityDTO.setCommodityCode(commodityCode);
-        return businessService.getStock(commodityDTO);
-    }
+//    @GetMapping("/getStock")
+//    ObjectResponse getStock(@RequestParam String commodityCode) {
+//        CommodityDTO commodityDTO = new CommodityDTO();
+//        commodityDTO.setCommodityCode(commodityCode);
+//        return businessService.getStock(commodityDTO);
+//    }
 
-    @GetMapping("/getAllStocks")
+//    @RequestMapping(value = "/stock", method = RequestMethod.GET)
+//    ObjectResponse getStock(@RequestParam(name = "commodityCode") String commodityCode) {
+//        CommodityDTO commodityDTO = new CommodityDTO();
+//        commodityDTO.setCommodityCode(commodityCode);
+//        return businessService.getStock(commodityDTO);
+//    }
+
+
+//    @GetMapping("/getAllStocks")
+//    ObjectResponse getAllStock() {
+//        log.info("Request All Stocks");
+//        return businessService.getAllStocks();
+//    }
+
+    @RequestMapping(value = "/stock", method = RequestMethod.GET)
     ObjectResponse getAllStock() {
         log.info("Request All Stocks");
         return businessService.getAllStocks();
     }
 
-    @GetMapping("/getOrderByOId")
-    ObjectResponse getOrderByOId(@RequestParam String orderNo) {
+    @RequestMapping(value = "/stock", method = RequestMethod.GET)
+    ObjectResponse getStock(@PathVariable(name = "commodityCode") String commodityCode) {
+        CommodityDTO commodityDTO = new CommodityDTO();
+        commodityDTO.setCommodityCode(commodityCode);
+        return businessService.getStock(commodityDTO);
+    }
+
+//    @GetMapping("/getOrderByOId")
+//    ObjectResponse getOrderByOId(@RequestParam String orderNo) {
+//        OrderDTO orderDTO = new OrderDTO();
+//        orderDTO.setOrderNo(orderNo);
+//        return businessService.getOrderByOId(orderDTO);
+//    }
+
+    @RequestMapping(value = "/order", method = RequestMethod.GET)
+    ObjectResponse getOrderByOId(@RequestParam(name = "orderNo") String orderNo) {
         OrderDTO orderDTO = new OrderDTO();
         orderDTO.setOrderNo(orderNo);
         return businessService.getOrderByOId(orderDTO);
     }
 
-    @GetMapping("/getOrderByUId")
-    ObjectResponse getOrderByUId(@RequestParam String userId) {
+//    @GetMapping("/getOrderByUId")
+//    ObjectResponse getOrderByUId(@RequestParam String userId) {
+//        AccountDTO accountDTO = new AccountDTO();
+//        accountDTO.setUserId(userId);
+//        return businessService.getOrderByUId(accountDTO);
+//    }
+
+    @RequestMapping(value = "/order/{userId}", method = RequestMethod.GET)
+    ObjectResponse getOrderByUId(@PathVariable(name = "userId") String userId) {
         AccountDTO accountDTO = new AccountDTO();
         accountDTO.setUserId(userId);
         return businessService.getOrderByUId(accountDTO);
